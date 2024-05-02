2eb05335f060a37a30d58282dd3b321acdff415d
Yogesh Thakur <yogeshth@infinite.com>
2024-05-02 08:49:03 +0530

    update review comment

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,11 +1,17 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
-	public static void main(String[] args) {
-
-		
-		int i = 10;
+	int i = 10;
+	
+	
+	By name = By.id("test");
+	
+	public void getName() {
+		System.out.println("name");
 	}
+	
 
 }
