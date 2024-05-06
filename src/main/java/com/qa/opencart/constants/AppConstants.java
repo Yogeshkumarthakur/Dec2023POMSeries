f565a11978f34c9cdb785e13fd56a2caf24ba1ae
Yogesh Thakur <yogeshth@infinite.com>
2024-05-06 14:53:39 +0530

    updated app constant

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -2,7 +2,7 @@ package com.qa.opencart.constants;
 
 public class AppConstants {
 	
-	public static final String LOGIN_PAGE_TITLE = "Account Login11";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
 	public static final String LOGIN_PAGE_URL_FRACTION = "route=account/login";
 	public static final String ACC_PAGE_URL_FRACTION = "route=account/account";
